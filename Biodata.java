92623d16da910442b8d883bcd0be007abc9ca5bf
Rachmad <108579144+rachmadrf@users.noreply.github.com>
2024-09-11 10:08:45 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,12 @@
+public class Biodata {
+
+    public static void main(String[] args) {
+        
+        System.out.println("-- Biodata Mahasiswa 1 --");
+        System.out.println("Nama\t: Rachmad Febriananda");
+        System.out.println("NIM\t: 244107020095");
+        System.out.println("Kelas\t: TI-1F");
+        System.out.println("Hobi\t: Mendengarkan musik"); 
+
+    }
+}
